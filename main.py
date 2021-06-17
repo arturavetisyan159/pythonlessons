5b39d2ca5718d9e7f71213c91d11bbca75415852
Artur <artur-avetisyan-90@inbox.ru>
2021-06-17 15:24:56 +0300

    class print

---
+++ main.py
@@ -0,0 +1,39 @@
+# работа с оператором print
+
+# name = str(input('What is yor name? '))
+# age = int(input('How old are u? '))
+# print('My name is', name + '. I am', str(age), 'years old.', sep=" ", end=" ")
+# print("Новая строка")
+
+
+# %s = string. %d = digital. %.2f = float(2 символа после запятой)
+
+# name = "Viktor"
+# age = 28
+# grade = 9.2
+# print("It is %s, %d. Level: %.3f" % (name, age, grade))
+
+
+# метод .format
+
+# print("This is a {0}. It is {1}".format("ball", "red"))
+
+
+# задача
+
+# print("Введите 4 числа: ")
+# frst = int(input("1: "))
+# scnd = int(input("2: "))
+# thrd = int(input("3: "))
+# frth = int(input("4: "))
+#
+# res1 = frst + scnd
+# res2 = thrd+ frth
+# result = res1 / res2
+# print("Ваш ответ: %.2f" % (result))
+
+
+# bool
+
+test = None
+print(test is None) # true
