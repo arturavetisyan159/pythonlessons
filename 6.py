da1e7f9e5386aad133f10417957c0fba02232c58
Artur <artur-avetisyan-90@inbox.ru>
2021-06-29 23:41:06 +0300

    task-3 last_commit

--- 6.py
+++ 6.py
@@ -1,18 +1,34 @@
-# Вывод символа
-
-n = int(input("Введите количество символов: "))
-symbol = input("Введите символ: ")
-print("0 - Горизонтальная")
-print("1 - Вертикальная")
-orientation = int(input("Выберите ориентацию: "))
-
-if orientation == 0:
-     while n > 0:
-         print(symbol, end=" ")
-         n -= 1
-elif orientation == 1:
-    while n > 0:
-        print(symbol)
-        n -= 1
+num = int(input("Введите число: "))
+is_even = 0
+number = str(num)
+while num > 0:
+    num = num // 10
+    is_even += 1
+first_part = ''
+second_part = ''
+if is_even % 2 == 0:
+    i = 0
+    j = int(len(number) / 2)
+    while i <= ((len(number) / 2) - 1):
+        first_part = first_part + number[i]
+        i += 1
+    while j <= len(number) - 1:
+        second_part = second_part + number[j]
+        j += 1
+    if first_part == second_part:
+        print(number + '- полиндром.')
+    else:
+        print(number + '- не полиндром.')
 else:
-    print("Неправильно выбрана ориентация!")
+    i = 0
+    j = int((len(number) + 1) / 2)
+    while i <= ((len(number) - 1) / 2) - 1:
+        first_part = first_part + number[i]
+        i += 1
+    while j <= len(number) - 1:
+        second_part = second_part  + number[j]
+        j += 1
+    if first_part == second_part:
+        print(number + '- полиндром.')
+    else:
+        print(number + '- не полиндром.')
