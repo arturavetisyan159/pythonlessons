069ee5767023f8fbe12dd87c8286338abfc0a7e6
Artur <artur-avetisyan-90@inbox.ru>
2021-07-15 19:56:01 +0300

    class work_weeks

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random as r
+
 # работа с оператором print
 
 # name = str(input('What is yor name? '))
@@ -65,3 +67,93 @@
 # print(my_list)
 
 # список[start:stop:step]
+
+# задача: остсортировать четные строки матрицы по возраст. , а нечетные по убыванию
+
+# mas = [[2, 5, 8],
+#        [5, 8, 2],
+#        [8, 7, 1],
+#        [0, 7, 1],
+#        [9, 11, 6]]
+#
+# for i in range(0, len(mas)):
+#     if i % 2 == 0 or i == 0:
+#         mas[i].sort()
+#     else:
+#         mas[i].sort(reverse=True)
+#
+# for row in mas:
+#     for m in row:
+#         print(m, end='\t')
+#     print()
+
+# наполнить список n уникальными рандомными элементами
+
+# n = int(input("размер списка: "))
+# my_list = []
+# for i in range(0, n):
+#     my_list.append(random.randrange(0, 21))
+# print(my_list)
+
+
+
+# w, h = 3, 4
+# matrix = [[r.randint(-20, 10) for x in range(w)] for y in range(h)]
+# multiply = 1
+# for row in matrix:
+#     for w in row:
+#         if w > 0:
+#             multiply *= w
+#         print(w, end='\t')
+#     print()
+# print('Произведение ненулевых элементов =', multiply)
+
+# Задача: составить список из элементов главной диагонали
+
+# w = int(input("Размерность массива: "))
+# h = w
+# print("Массив из случайных чисел от 1 до 100: ")
+# matrix = [[r.randint(1, 101) for x in range(h)] for y in range(h)]
+# my_list = []
+#
+# for row in matrix:
+#     for j in row:
+#         print(j, end='\t')
+#     print()
+#
+# for i in range(0, w):
+#     my_list.append(matrix[i][i])
+# print(my_list)
+
+# Задача: поменять местами 1-ую и 2-ую, 3-ую и 4-ую, 5-ую и 6-ую.
+# w, h = 6, 6
+# matrix = [[r.randint(1, 10) for x in range(w)] for y in range(h)]
+#
+# for row in matrix:
+#     for j in row:
+#         print(j, end='\t')
+#     print()
+#
+# print()
+#
+# for h in range(len(matrix)):
+#     if h % 2 == 0 and h != len(matrix):
+#         for w in range(len(matrix[h])):
+#             print(matrix[h + 1][w], end='\t')
+#         print()
+#         for w in range(len(matrix[h])):
+#             print(matrix[h][w], end='\t')
+#         print()
+
+
+# Задача: вывести матрицу рабочих дней.
+# days = [d for d in range(1, 32)]
+#
+# weeks = [days[i: i + 7]for i in range(0, len(days), 7)]
+#
+# work_weeks = [week[0:5] for week in weeks]
+#
+# for row in work_weeks:
+#     for x in row:
+#         print(x, end='\t')
+#     print()
