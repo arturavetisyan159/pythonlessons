59e73015c956e5f2192d30af45beb0ee0b3979e0
Artur <artur-avetisyan-90@inbox.ru>
2021-07-29 20:14:42 +0300

    class- 22 class_finish

--- main.py
+++ main.py
@@ -991,7 +991,87 @@ locale.setlocale(locale.LC_ALL, "ru_RU")
 
 ##############################
 # Задание
-d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
-d_new = {'name': d.pop('name'), 'salary': d.pop('salary')}
-print('новый словарь:', d_new)
-print('исходный словарь:', d)
+# d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
+# d_new = {}
+# d_new['name'] = d.pop('name')
+# d_new['salary'] = d.pop('salary')
+# print('новый словарь:', d_new)
+# print('исходный словарь:', d)
+
+##############################
+# Задание: заменить 'city' на 'location'
+# d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
+# city = d.pop('city')
+# d.update([('location', city)])
+# print(d)
+
+##############################
+# a = {
+#     "first": {
+#         1: 'one',
+#         2:'two',
+#         3:'three'
+#     },
+#     "second": {
+#         4: 'four',
+#         5: 'five'
+#     }
+# }
+# # print(a["first"][2]) # two
+#
+# for x in a:
+#     print()
+#     print(x, ':', sep='')
+#     for y in a[x]:
+#         print(y, ':', ' ', a[x][y], sep='')
+
+##############################
+# Задание:
+
+# sellings = {
+#     'John': {
+#         'N': 3056,
+#         'S': 8463,
+#         'E': 8441,
+#         'W': 2694
+#     },
+#     'Tom': {
+#         'N': 4835,
+#         'S': 6786,
+#         'E': 4737,
+#         'W': 3612
+#     },
+#     'Anne': {
+#         'N': 5239,
+#         'S': 4802,
+#         'E': 5820,
+#         'W': 1859
+#     },
+#     'Fiona': {
+#         'N': 3904,
+#         'S': 3645,
+#         'E': 8821,
+#         'W': 2451
+#     }
+# }
+# for x in sellings:
+#     print()
+#     print(x, ':', sep='')
+#     for y in sellings[x]:
+#         print(y, ':', ' ', sellings[x][y], sep='')
+#
+# print()
+# print()
+# name = input('Имя: ')
+# region = input('Регион: ')
+# print(sellings[name][region])
+# new_value = int(input('Новое значение: '))
+# sellings[name][region] = new_value
+#
+# print(sellings[name])
+
+##############################
+
+
+
+
