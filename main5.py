abf32e1a76680f63a05194ecdf889180ae24910f
Artur <artur-avetisyan-90@inbox.ru>
2021-10-19 00:29:11 +0300

    class acc add set and get

--- main5.py
+++ main5.py
@@ -391,76 +391,105 @@ import math
 #     suffix = "RUB"
 #     suffix_usd = "USD"
 #     suffix_eur = "EUR"
-#
+
 #     def __init__(self, surname, num, percent, value=0):
-#         self.surname = surname
-#         self.num = num
-#         self.percent = percent
-#         self.value = value
-#         self.value = value
-#         print(f"Счет #{self.num}, принадлежащий {self.surname}, был октрыт")
+#         self.__surname = surname
+#         self.__num = num
+#         self.__percent = percent
+#         self.__value = value
+#         print(f"Счет #{self.__num}, принадлежащий {self.__surname}, был октрыт")
 #         print("*" * 50)
-#
+
 #     def __del__(self):
 #         print("*" * 50)
-#         print(f"Счет #{self.num}, принадлежащий {self.surname}, был закрты")
-#
+#         print(f"Счет #{self.__num}, принадлежащий {self.__surname}, был закрты")
+
 #     @classmethod
 #     def set_usd_rate(cls, rate):
 #         cls.rate_usd = rate
-#
+
 #     @classmethod
 #     def set_eur_rate(cls, rate):
 #         cls.rate_eur = rate
-#
+
 #     @staticmethod
 #     def convert(value, rate):
 #         return value * rate
-#
+
+#     @property
+#     def num(self):
+#         return self.__num
+    
+#     @num.setter
+#     def num(self, n):
+#         self.__num = n
+
+#     @property
+#     def surname(self):
+#         return self.__surname
+    
+#     @surname.setter
+#     def surname(self, s):
+#         self.__surname = s
+
+#     @property
+#     def percent(self):
+#         return self.percent
+
+#     @percent.setter
+#     def percent(self, p):
+#         self.__percent = p
+
+#     @property
+#     def value(self):
+#         return self.__value
+
+#     @value.setter
+#     def value(self, v):
+#         self.__value = v
+
 #     def print_balance(self):
-#         print(f"Текущий баланс: {self.value} {Account.suffix}")
-#
-#     def edit_owner(self, surname):
-#         self.surname = surname
-#
+#         print(f"Текущий баланс: {self.__value} {Account.suffix}")
+
 #     def print_info(self):
 #         print("Информация о счете:")
 #         print("-" * 20)
-#         print(f"#{self.num}")
-#         print(f"Владелец: {self.surname}")
+#         print(f"#{self.__num}")
+#         print(f"Владелец: {self.__surname}")
 #         self.print_balance()
-#         print(f"Процент: {self.percent : .0%}")
+#         print(f"Процент: {self.__percent : .0%}")
 #         print("-" * 20)
-#
+
 #     def convert_to_usd(self):
 #         usd_val = Account.convert(self.value, Account.rate_usd)
 #         print(f"Состояние счета: {usd_val} {Account.suffix_usd}.")
-#
+
 #     def convert_to_eur(self):
-#         eur_val = Account.convert(self.value, Account.rate_eur)
+#         eur_val = Account.convert(self.__value, Account.rate_eur)
 #         print(f"Состояние счета: {eur_val} {Account.suffix_eur}")
-#
+
 #     def add_percents(self):
-#         self.value += self.value * self.percent
+#         self.__value += self.__value * self.__percent
 #         print("Проценты были успешно начислены!")
 #         self.print_balance()
-#
+
 #     def withdraw_money(self, val):
-#         if val > self.value:
+#         if val > self.__value:
 #             print(f"К сожалению у вас нет {val} {Account.suffix}.")
 #         else:
-#             self.value = self.value - val
+#             self.__value = self.__value - val
 #             print(f"{val} {Account.suffix} были успешно сняты.")
 #             self.print_balance()
-#
+
 #     def add_money(self, val):
-#         self.value += val
+#         self.__value += val
 #         print(f"{val} {Account.suffix} быди успешно добавлены.")
 #         self.print_balance()
-#
-#
+
+
 # acc = Account("Долгих", 12321, 0.03, 1000)
 # acc.print_info()
+# acc.num = 1984
 # acc.convert_to_usd()
 # acc.convert_to_eur()
 # print()
@@ -468,7 +497,9 @@ import math
 # Account.set_eur_rate(4)
 # acc.convert_to_usd()
 # acc.convert_to_eur()
-# acc.edit_owner("Саяхов")
+# acc.surname = "Иванов"
+# acc.percent = 0.15
+# acc.value = 2000
 # acc.print_info()
 # print()
 # acc.add_percents()
@@ -525,54 +556,54 @@ import math
 # print(line.__dict__)
 # line.draw_line()
 
-class Figure:
-    def __init__(self, color):
-        self.__color = color
+# class Figure:
+#     def __init__(self, color):
+#         self.__color = color
 
-    @property
-    def color(self):
-        return self.__color
+#     @property
+#     def color(self):
+#         return self.__color
 
-    @color.setter
-    def color(self, c):
-        self.__color = c
+#     @color.setter
+#     def color(self, c):
+#         self.__color = c
 
 
-class Rectangle(Figure):
-    def __init__(self, width, height, color):
-        super().__init__(color)
-        self.__width = width
-        self.__height = height
+# class Rectangle(Figure):
+#     def __init__(self, width, height, color):
+#         super().__init__(color)
+#         self.__width = width
+#         self.__height = height
 
-    @property
-    def width(self):
-        return self.__width
+#     @property
+#     def width(self):
+#         return self.__width
 
-    @width.setter
-    def width(self, w):
-        if w > 0:
-            self.__width = w
-        else:
-            raise ValueError
+#     @width.setter
+#     def width(self, w):
+#         if w > 0:
+#             self.__width = w
+#         else:
+#             raise ValueError
 
-    @property
-    def height(self):
-        return self.__height
+#     @property
+#     def height(self):
+#         return self.__height
 
-    @height.setter
-    def height(self, h):
-        if h > 0:
-            self.__height = h
-        else:
-            raise ValueError
+#     @height.setter
+#     def height(self, h):
+#         if h > 0:
+#             self.__height = h
+#         else:
+#             raise ValueError
 
-    def area(self):
-        return f"Площадь равна {self.__height * self.__width}"
+#     def area(self):
+#         return f"Площадь равна {self.__height * self.__width}"
 
-rect = Rectangle(10, 20, "green")
-print(rect.width)
-rect.width = 11
-print(rect.width)
+# rect = Rectangle(10, 20, "green")
+# print(rect.width)
+# rect.width = 11
+# print(rect.width)
 
     
 
